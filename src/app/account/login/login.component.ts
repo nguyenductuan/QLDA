afd663695598a31decc34e9e484f55ae394d2efc
nguyenductuan <nguyenductuanbk1234@gmail.com>
2025-05-17 14:36:43 +0700

    edit code

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,64 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { UserService } from '../../service/user.service';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { UserinfoService } from '../../service/userinfo.service';
 
+import { UserService } from '../../service/user.service';
+import { UserinfoService } from '../../service/userinfo.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  formLogin: FormGroup;
-  data: any;
- user:any;
-errorMessage: string = '';
-constructor(public  app:UserService,public userinfo: UserinfoService, public router: Router, private snackBar: MatSnackBar){}
+export class LoginComponent implements OnInit {
+  formLogin!: FormGroup;
+  errorMessage:String;
+
+  constructor(
+    private userService: UserService,
+    private userinfoService: UserinfoService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
+
   ngOnInit(): void {
     this.formLogin = new FormGroup({
-     email: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required),
-    }
-    )
+    });
   }
-  login(){
-    if (this.formLogin.invalid) {
-      return;
-    }
-this.app.login(this.formLogin.value).subscribe(res => {
-this.data = res
-console.log(this.data)
-if(this.data.data == '')
-{
-  this.snackBar.open(this.data.message, 'Đóng', {
-    duration: 3000,
-    horizontalPosition: 'right',
-    verticalPosition: 'top'
-  });
-}
-else
-{
 
+  login(): void {
+    if (this.formLogin.invalid) return;
 
-    if(this.data.data[0].role.name == 'User')
-      {
-        this.router.navigate(['/home']);
-        
-      }
-      if(this.data.data[0].role.name == 'Admin')
-      {
-        this.router.navigate(['/admin']);
-        
+    this.userService.login(this.formLogin.value).subscribe(response => {
+      const { data, message } = response;
+
+      if (!data || data.length === 0) {
+        this.showSnackbar(message);
+        return;
       }
 
-      this.userinfo.setUserInfo(this.data.data[0]);
+      const user = data[0];
+      this.userinfoService.setUserInfo(user);
 
-}
-})
+      switch (user.role?.name) {
+        case 'User':
+          this.router.navigate(['/home']);
+          break;
+        case 'Admin':
+          this.router.navigate(['/admin']);
+          break;
+        default:
+          this.showSnackbar('Không xác định được vai trò người dùng.');
+      }
+    });
+  }
+
+  private showSnackbar(message: string): void {
+    this.snackBar.open(message, 'Đóng', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top'
+    });
   }
 }
-  
-
